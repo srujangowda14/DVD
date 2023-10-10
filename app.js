73b492d92667ce4101654ea024bf60b7fd10f281
Srujan Gowda <Srujan.GowdaSJ@fmr.com>
2023-10-10 16:28:01 +0530

    Filter added

--- app.js
+++ app.js
@@ -24,9 +24,10 @@ app.use(function(req, res, next) {
 
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
+    console.error(err.stack);
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+    
     // render the error page
     res.status(err.status || 500);
     res.json('error');
--- main/dvd.js
+++ main/dvd.js
@@ -1,61 +1,44 @@
 var express = require('express');
 const fs = require('fs');
 
- 
 
 var team = require('../data/team')
 var router = express.Router();
 
  
-
- 
-
 let read_json_file = () => {
     let file = './data/dvd.json';
     return fs.readFileSync(file);
 }
 
- 
-
 exports.list = () => {
     return JSON.parse(read_json_file());
 };
 
- 
-
 function computePrice(json, percent) {
-    taxPercentage = taxPercentage / 100;
-    for (let book of json) {
-        console.log(book.price);
-        book.price =book.price+ ( book.price * (percent));
-        book.taxPercentage = percent;
-        console.log(book.price);
+    taxPercentage = percent / 100;
+    for (let dvd of json) {
+        console.log(dvd.price);
+        dvd.price =dvd.price+ ( dvd.price * (percent));
+        dvd.taxPercentage = percent;
+        console.log(dvd.price);
     }
     console.log(json);
     return json
 }
 
- 
-
- 
-
 /* GET users listing. */
 router.get('/all/:location?', function(req, res, next) {
     let json_result = JSON.parse(read_json_file());
-
- 
-
     let location = req.params.location;
     if (location) {
         console.log(`received location: ${location}`);
         if (location === 'IN') {
             json_result = computePrice(json_result, 18);
         } else if (location === 'IR') {
-            console.log("TEST");
             json_result = computePrice(json_result, 23);
         } 
         else if (location === 'US-NC') {
-            console.log("TEST");
             json_result = computePrice(json_result, 8);
         }else {
             console.log('not a valid location');
@@ -65,12 +48,32 @@ router.get('/all/:location?', function(req, res, next) {
 });
 
 router.get('/search',(req,res,next)=>{
-    const {minprice, maxprice, rating, brand} = req.params;
+    const {minprice, maxprice, rating, brand} = req.query;
 
-    let jsonResult = JSON.parse(read_json_file);
+    let jsonResult = JSON.parse(read_json_file());
 
-    
-})
+    const filteredData = jsonResult.filter((dvd) => {
+ 
+    if (minprice !== undefined && dvd.price < minprice) {
+      return false;
+    }
+
+    if (maxprice !== undefined && dvd.price > maxprice) {
+       return false;
+    }
+
+    if (rating !== undefined && dvd.rating < rating) {
+       return false;
+    }
+
+    if (brand !== undefined && dvd.brand !== brand) {
+       return false;
+    }
+
+    return true;
+   });
+    res.json(filteredData);  
+});
 
  
 
